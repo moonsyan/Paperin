105b86617b51f1bf67a75093004be456b5f27127
ming <1367992691@qq.com>
2026-09-13 11:56:40 +0800

    last

--- design/soft-workbench/Icon.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Icon({ name }: { name: string }): JSX.Element {
-  return <svg className="icon" aria-hidden="true" viewBox="0 0 256 256"><use href={`#i-${name}`} /></svg>
-}
